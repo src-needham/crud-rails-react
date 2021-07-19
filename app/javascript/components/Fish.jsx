b90cc6fdc66a7ea8d948f9660baea39dfd586fe5
Sam Needham <src.needham@gmail.com>
2021-07-19 17:15:12 +0100

    Completes Fish jsx component

--- app/javascript/components/Fish.jsx
+++ app/javascript/components/Fish.jsx
@@ -1,74 +1,109 @@
-import { message, Popconfirm } from "antd";
+import { Table, message, Popconfirm } from "antd";
+import React from "react";
+import AddFishModal from "./AddFishModal";
 
-state = {
-  fish: [],
-};
+class Fish extends React.Component{
+  columns = [
+    {
+      title: "Common Name",
+      dataIndex: "common_name",
+      key: "common_name",
+    },
+    {
+      title: "Scientific Name",
+      dataIndex: "scientific_name",
+      key: "scientific_name",
+    },
+    {
+      title: "pH",
+      dataIndex: "ph",
+      key: "ph",
+    },
+    {
+      title: "Quantity",
+      dataIndex: "quantity",
+      key: "quantity",
+    },
+    {
+      title: "",
+      key: "action",
+      render: (_text, record) => (
+        <Popconfirm title="Are you sure you want to delete this fish?" onConfirm={() => this.deleteBeer(record.id)} okText="Yes" cancelText="No">
+          <a href="#" type="danger">
+            Delete{" "}
+          </a>
+        </Popconfirm>
+      ),
+    },
+  ];
 
-loadFish = () => {
-  const url = "api/v1/fish/index";
-  fetch(url)
+
+  state = {
+    fish: [],
+  };
+
+  componentDidMount() {
+    this.loadFish();
+  }
+
+  loadFish = () => {
+    const url = "api/v1/fish/index";
+    fetch(url)
+      .then((data) => {
+        if (data.ok) {
+          return data.json();
+        }
+        throw new Error("Network error.");
+      })
+      .then((data) => {
+        data.forEach((fish) => {
+          const newEl = {
+            key: fish.id,
+            id: fish.id,
+            common_name: fish.common_name,
+            scientific_name: fish.scientific_name,
+            ph: fish.ph,
+            quantity: fish.quantity,
+          };
+
+          this.setState((prevState) => ({
+            fish: [...prevState.fish, newEl],
+          }));
+        });
+      })
+      .catch((err) => message.error("Error: " + err));
+  }
+
+  deleteFish = (id) => {
+    const url = `api/v1/fish/${id}`;
+
+    fetch(url, {
+      method: "delete",
+    })
     .then((data) => {
-      if (data.ok) {
+      if(data.ok) {
+        this.reloadFish();
         return data.json();
       }
-      throw new Error("Network error.");
-    })
-    .then((data) => {
-      data.forEach((fish) => {
-        const newEl = {
-          key: fish.id,
-          id: fish.id,
-          common_name: fish.common_name,
-          scientific_name: fish.scientific_name,
-          ph: fish.ph,
-          quantity: fish.quantity,
-        };
-
-        this.setState((prevState) => ({
-          fish: [...prevState.fish, newEl],
-        }));
-      });
+      throw new Error("Network Error.");
     })
     .catch((err) => message.error("Error: " + err));
+  };
+
+  reloadFish = () => {
+    this.setState({ fish: [] });
+    his.loadFish();
+  };
+
+  render() {
+    return (
+      <>
+        <Table className="table-striped-rows" dataSource={this.state.fish} columns={this.columns} pagination={{ pagesize: 5}} />
+      
+        <AddFishModal reloadFish={this.reloadFish} />
+      </>
+    );
+  }
 }
 
-columns = [
-  {
-    title: "Fish",
-    dataIndex: "common_name",
-    key: "common_name",
-  },
-
-  ...{
-    title: "",
-    key: "action",
-    render: (_text, record) => (
-      <Popconfirm title="Are you sure you want to delete this fish?" onConfirm={() => this.deleteFish(record.id)} okText="Yes" cancelText="No">
-        <a href="#" type="danger">
-          Delete{" "}
-        </a>
-        </Popconfirm>      
-    ),
-  },
-];
-
-deleteFish = (id) => {
-  const url = `api/v1/fish/${id}`;
-
-  fetch(url, {
-    method: "delete",
-  })
-  .then((data) => {
-    if(data.ok) {
-      this.reloadFish();
-      return data.json();
-    }
-    throw new Error("Network Error.");
-  })
-  .catch((err) => message.error("Error: " + err));
-};
-
-reloadFish = () => {
-  this.setState({ fish: [] });
-  this.loadFish();
-};
+export default Fish;
